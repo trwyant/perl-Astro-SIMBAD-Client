ec28fb6441c5f5d935f35a54172ea665b5bf75f6
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-08-09 19:19:49 +0000

    Movethe author tests from xt/ to xt/author/.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SIMBAD-Client/trunk@1903 f6fd3633-1913-47c2-bad4-372d75c948ec

--- inc/Astro/SIMBAD/Client/Build.pm
+++ inc/Astro/SIMBAD/Client/Build.pm
@@ -14,7 +14,7 @@ sub ACTION_authortest {
     my ( $self, @args ) = @_;
 
     $self->depends_on( 'build' );
-    $self->test_files( qw{ t xt } );
+    $self->test_files( qw{ t xt/author } );
     $self->depends_on( 'test' );
 
     return;
@@ -77,7 +77,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009, Thomas R. Wyant, III
+Copyright (C) 2009-2010 Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
