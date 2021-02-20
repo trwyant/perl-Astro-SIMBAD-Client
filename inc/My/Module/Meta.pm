37accc98b2a45d3c4fc26202844456cf7ac721dd
Tom Wyant <wyant@cpan.org>
2021-02-20 11:04:23 -0500

    Add config_requires to metadata.
    Add add_to_cleanup() to inc/My/Module/Meta.pm.  Also version_from().

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -17,6 +17,18 @@ sub new {
     return $self;
 }
 
+sub abstract {
+    return 'Fetch astronomical data from SIMBAD 4.';
+}
+
+sub add_to_cleanup {
+    return [ qw{ cover_db xt/author/optionals } ];
+}
+
+sub author {
+    return 'Tom Wyant (wyant at cpan dot org)';
+}
+
 sub build_requires {
     return +{
 	'Data::Dumper'	=> 0,
@@ -25,9 +37,28 @@ sub build_requires {
     };
 }
 
+sub configure_requires {
+    return +{
+	'Config'	=> 0,
+	'Getopt::Std'	=> 0,
+	'lib'	=> 0,
+	'strict'	=> 0,
+	'warnings'	=> 0,
+    };
+}
+
+sub dist_name {
+    return 'Astro-SIMBAD-Client';
+}
+
 sub distribution {
     my ( $self ) = @_;
     return $self->{distribution};
+}
+
+
+sub license {
+    return 'perl';
 }
 
 sub meta_merge {
@@ -36,14 +67,12 @@ sub meta_merge {
 	'meta-spec'	=> {
 	    version	=> 2,
 	},
-	no_index => {
-	    directory => [ qw{ inc t xt } ],
-	},
-	resources => {
+	dynamic_config	=> 1,
+	resources	=> {
 	    bugtracker	=> {
 		web	=> 'https://github.com/trwyant/perl-Astro-SIMBAD-Client/issues',
-#                mailto  => 'wyant@cpan.org',
-            },
+		mailto  => 'wyant@cpan.org',
+	    },
 	    license	=> 'http://dev.perl.org/licenses/',
 	    repository	=> {
 		type	=> 'git',
@@ -52,6 +81,21 @@ sub meta_merge {
 	    },
 	},
 	@extra,
+    };
+}
+
+
+sub module_name {
+    return 'Astro::SIMBAD::Client';
+}
+
+sub no_index {
+    return +{
+      directory => [
+                     'inc',
+                     't',
+                     'xt',
+                   ],
     };
 }
 
@@ -91,6 +135,16 @@ sub requires_perl {
 }
 
 
+sub script_files {
+    return [
+	'script/simbadc',
+    ];
+}
+
+sub version_from {
+    return 'lib/Astro/SIMBAD/Client.pm';
+}
+
 1;
 
 __END__
@@ -124,6 +178,19 @@ This class supports the following public methods:
 
 This method instantiates the class.
 
+=head2 abstract
+
+This method returns the distribution's abstract.
+
+=head2 add_to_cleanup
+
+This method returns a reference to an array of files to be added to the
+cleanup.
+
+=head2 author
+
+This method returns the name of the distribution author
+
 =head2 build_requires
 
  use YAML;
@@ -133,6 +200,20 @@ This method computes and returns a reference to a hash describing the
 modules required to build the C<Astro::SIMBAD::Client> package, suitable
 for use in a F<Build.PL> C<build_requires> key, or a F<Makefile.PL>
 C<< {META_MERGE}->{build_requires} >> or C<BUILD_REQUIRES> key.
+
+=head2 configure_requires
+
+ use YAML;
+ print Dump( $meta->configure_requires() );
+
+This method returns a reference to a hash describing the modules
+required to configure the package, suitable for use in a F<Build.PL>
+C<configure_requires> key, or a F<Makefile.PL>
+C<< {META_MERGE}->{configure_requires} >> or C<CONFIGURE_REQUIRES> key.
+
+=head2 dist_name
+
+This method returns the distribution name.
 
 =head2 distribution
 
@@ -145,6 +226,10 @@ C<< {META_MERGE}->{build_requires} >> or C<BUILD_REQUIRES> key.
 This method returns the value of the environment variable
 C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
 
+=head2 license
+
+This method returns the distribution's license.
+
 =head2 meta_merge
 
  use YAML;
@@ -152,9 +237,20 @@ C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
 
 This method returns a reference to a hash describing the meta-data which
 has to be provided by making use of the builder's C<meta_merge>
-functionality. This includes the C<no_index> and C<resources> data.
+functionality. This includes the C<dynamic_config> and C<resources>
+data.
 
 Any arguments will be appended to the generated array.
+
+=head2 module_name
+
+This method returns the name of the module the distribution is based
+on.
+
+=head2 no_index
+
+This method returns the names of things which are not to be indexed
+by CPAN.
 
 =head2 provides
 
@@ -183,6 +279,16 @@ true, configuration-specific modules may be added.
  print 'This package requires Perl ', $meta->requires_perl(), "\n";
 
 This method returns the version of Perl required by the package.
+
+=head2 script_files
+
+This method returns a reference to an array containing the names of
+script files provided by this distribution. This array may be empty.
+
+=head2 version_from
+
+This method returns the name of the distribution file from which the
+distribution's version is to be derived.
 
 =head1 ATTRIBUTES
 
